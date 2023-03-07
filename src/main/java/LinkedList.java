87b5d88b31b004ef9a114abbd7556f2f4aa2d3c0
OzgurTY <ozgur.yavuz@ozu.edu.tr>
2023-03-07 15:44:18 +0300

    Added LinkedList and Node classes.

--- src/main/java/LinkedList.java
+++ src/main/java/LinkedList.java
@@ -6,4 +6,25 @@ public class LinkedList {
         head = null;
         tail = null;
     }
+
+    public void insertFirst(Node newNode){
+        if(tail == null){
+            tail = newNode;
+        }
+        newNode.setNext(head);
+        head = newNode;
+    }
+
+    public void insertLast(Node newNode){
+        if(head == null){
+            head = newNode;
+        }
+        tail.setNext(newNode);
+        tail = newNode;
+    }
+
+    public void insertMiddle(Node newNode, Node previousNode){
+        newNode.setNext(previousNode.getNext());
+        previousNode.setNext(newNode);
+    }
 }
